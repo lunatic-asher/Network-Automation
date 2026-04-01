379942edf6e6298621900c3e1b92064cf1309409
Ash <ashervinukonda@gmail.com>
2026-04-01 17:02:38 +0000

    telnet version 1.1 using the getpass lib

--- Automation - Python/Day-2: Logging into device using Telnet/Telnet-1.py
+++ Automation - Python/Day-2: Logging into device using Telnet/Telnet-1.py
@@ -1,1 +1,21 @@
- 
+import telnetlib
+import getpass
+
+switch_ip = '192.168.99.30'
+
+print(f"Logging into {switch_ip}")
+
+username = input("Enter your username: ")
+Password = getpass.getpass()
+
+tn = telnetlib.Telnet(switch_ip)
+
+tn.read_until(b"login:")
+tn.write(username.encode('ascii') + b'\n')>
+tn.read_until(b"Password:")
+tn.write(Password.encode('ascii') + b'\n')
+tn.write (b'show version\n')
+tn.write(b'exit\n')
+
+print(tn.read_all().decode('ascii'))
+
